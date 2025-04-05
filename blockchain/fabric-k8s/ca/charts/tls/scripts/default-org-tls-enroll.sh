dc37d0fc7a11e22e7848948790957195fb24aed4
nickrudy <nik.datascience@gmail.com>
2025-04-05 22:36:54 +0100

    fix: move enrollment logic to script file

---
+++ blockchain/fabric-k8s/ca/charts/tls/scripts/default-org-tls-enroll.sh
@@ -0,0 +1,79 @@
+#!/bin/sh
+
+clean_msp() {
+  local msp_dir=$1
+  echo "🧹 Cleaning MSP at $msp_dir"
+
+  rm -rf ${msp_dir}/keystore/*
+  rm -rf ${msp_dir}/signcerts/*
+  rm -rf ${msp_dir}/tlscacerts/*
+}
+
+export CA_HOST="{{ .Values.name }}.${ORG_DOMAIN}:7054"
+export NODE_MSPDIR="{{ .Values.node.type}}s/{{ .Values.node.name }}.${ORG_DOMAIN}/tls"
+
+echo "⏳ Waiting for TLS CA to become ready..."
+sleep 10
+
+# Admin enrollment
+clean_msp "${FABRIC_CA_CLIENT_HOME}/tlsca/{{ .Values.admin.user }}/msp"
+fabric-ca-client enroll \
+  -u "https://{{ .Values.admin.user }}:{{ .Values.admin.pass }}@${CA_HOST}" \
+  --tls.certfiles "${FABRIC_CA_HOME}/ca-cert.pem" \
+  --enrollment.profile tls \
+  --mspdir "tlsca/{{ .Values.admin.user }}/msp" \
+  -d
+
+# Register Org bootstrap identity
+fabric-ca-client register \
+  --id.name "{{ .Values.org.boot.name }}" \
+  --id.secret "{{ .Values.org.boot.secret }}" \
+  -u "https://${CA_HOST}" \
+  --tls.certfiles "${FABRIC_CA_HOME}/ca-cert.pem" \
+  --mspdir "tlsca/{{ .Values.admin.user }}/msp" \
+  -d
+
+# Enroll Org bootstrap identity
+clean_msp "${FABRIC_CA_CLIENT_HOME}/tlsca/{{ .Values.org.boot.name }}/msp"
+fabric-ca-client enroll \
+  -u "https://{{ .Values.org.boot.name }}:{{ .Values.org.boot.secret }}@${CA_HOST}" \
+  --tls.certfiles "${FABRIC_CA_HOME}/ca-cert.pem" \
+  --enrollment.profile tls \
+  --csr.hosts "*.{{ .Values.namespace }}.svc.cluster.local,localhost" \
+  --mspdir "tlsca/{{ .Values.org.boot.name }}/msp" \
+  -d
+
+# Rename Org bootstrap key
+KEY_PATH="${FABRIC_CA_CLIENT_HOME}/tlsca/{{ .Values.org.boot.name }}/msp/keystore"
+mv ${KEY_PATH}/* "${KEY_PATH}/key.pem"
+
+# Register node
+fabric-ca-client register \
+  --id.name "{{ .Values.node.name }}" \
+  --id.secret "{{ .Values.node.secret }}" \
+  --id.type "{{ .Values.node.type }}" \
+  -u "https://${CA_HOST}" \
+  --tls.certfiles "${FABRIC_CA_HOME}/ca-cert.pem" \
+  --mspdir "tlsca/{{ .Values.admin.user }}/msp" \
+  -d
+
+# Enroll node
+clean_msp "${FABRIC_CA_CLIENT_HOME}/${NODE_MSPDIR}"
+fabric-ca-client enroll \
+  -u "https://{{ .Values.node.name }}:{{ .Values.node.secret }}@${CA_HOST}" \
+  --enrollment.profile tls \
+  --csr.hosts "{{ .Values.node.name }}.{{ .Values.namespace }}.svc.cluster.local,localhost" \
+  --tls.certfiles "${FABRIC_CA_HOME}/ca-cert.pem" \
+  --mspdir "${NODE_MSPDIR}" \
+  -d
+
+echo "⏳ Waiting for Nodes Private Key to become ready..."
+sleep 5
+
+# Rename node key and certs
+KEY_PATH="${FABRIC_CA_CLIENT_HOME}/${NODE_MSPDIR}"
+mv ${KEY_PATH}/keystore/* "${KEY_PATH}/server.key"
+mv ${KEY_PATH}/signcerts/cert.pem "${KEY_PATH}/server.crt"
+cp "${FABRIC_CA_HOME}/ca-cert.pem" "${KEY_PATH}/ca.crt"
+
+echo "✅ TLS enrollment and registration complete"
